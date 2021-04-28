11eb35c5a12d9849dc826116a75fd939745d91c5
12189108 <zhoumeng200012@163.com>
2021-04-28 23:58:27 +0800

    修复无法修改请求头的BUG

--- Support/java/HttpSupport.java
+++ Support/java/HttpSupport.java
@@ -7,29 +7,43 @@ import java.util.*;
 public class HttpSupport
 {
 	private Map<String,List<String>> HeaderFields;
-	
+	private URL url;
+	private String Url;
+	private HttpURLConnection conn;
+	public HttpSupport init(String Url){
+		this.Url=Url;
+		try {
+			url=new URL(Url);
+			conn=(HttpURLConnection) url.openConnection();
+			conn.setUseCaches(false);
+			conn.setInstanceFollowRedirects(true);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return this;
+	}
 	public HttpSupport AttachCookie(String Cookie){
-		if(Cookie!=null)System.setProperty("Cookie",Cookie);
+		if(Cookie!=null)conn.setRequestProperty("Cookie",Cookie);
 		return this;
 	}
 	
 	public HttpSupport AttachContent_type(String content_type){
-		if(content_type!=null)System.setProperty("Content-Type",content_type);
+		if(content_type!=null)conn.setRequestProperty("Content-Type",content_type);
 		return this;
 	}
 	
 	public HttpSupport AttachUser_Agent(String User_Agent){
-		if(User_Agent!=null)System.setProperty("User-Agent",User_Agent);
+		if(User_Agent!=null)conn.setRequestProperty("User-Agent",User_Agent);
 		return this;
 	}
 	
 	public HttpSupport AttachAccept_Encoding(String Accept_Encoding){
-		if(Accept_Encoding!=null)System.setProperty("Accept-Encoding",Accept_Encoding);
+		if(Accept_Encoding!=null)conn.setRequestProperty("Accept-Encoding",Accept_Encoding);
 		return this;
 	}
 	
 	public HttpSupport AttachProperty(String key,String value){
-		if(key!=null&&value!=null)System.setProperty(key,value);
+		if(key!=null&&value!=null)conn.setRequestProperty(key,value);
 		return this;
 	}
 	
@@ -38,15 +52,10 @@ public class HttpSupport
 		else return null;
 	}
 	
-	public String[] getHtml(String Url){
+	public String[] getHtml(){
 		if(URLUtil.isNetworkUrl(Url)){
 			try
 			{
-				URL url=new URL(Url);
-				HttpURLConnection conn=(HttpURLConnection) url.openConnection();
-				conn.setUseCaches(false);
-				conn.setInstanceFollowRedirects(true);
-				conn.connect();
 				int responseCode=conn.getResponseCode();
 				BufferedReader reader=new BufferedReader(new InputStreamReader(conn.getInputStream()));
 				StringBuilder content=new StringBuilder();
@@ -68,12 +77,10 @@ public class HttpSupport
 		}	
 	}
 	
-	public String[] POSTData(String Url,String postdata){
+	public String[] POSTData(String postdata){
 		if(URLUtil.isNetworkUrl(Url)){
 			try
 			{
-				URL url=new URL(Url);
-				HttpURLConnection conn=(HttpURLConnection) url.openConnection();
 				conn.setDoOutput(true);
 				conn.setDoInput(true);
 				conn.setRequestMethod("POST");
